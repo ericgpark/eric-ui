9c342f272763637a9f6a4bb4290d7fefd7885fb6
ericgpark <ericparkmusic@gmail.com>
2025-08-27 17:04:07 -0700

    Add default fonts

---
+++ src/theme/fonts.ts
@@ -0,0 +1,6 @@
+export const defaultFonts = {
+  heading: 'var(--eric-ui-font-heading, system-ui, sans-serif)',
+  subheading: 'var(--eric-ui-font-subheading, system-ui, sans-serif)',
+  body: 'var(--eric-ui-font-body, system-ui, sans-serif)',
+  caption: 'var(--eric-ui-font-caption, system-ui, sans-serif)',
+};
