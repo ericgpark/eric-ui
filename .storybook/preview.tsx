1a72541bfad3ea514f1db1068d9e3f2068ca3c27
ericgpark <ericparkmusic@gmail.com>
2025-08-29 11:53:12 -0700

    Set up global styling variables

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react-vite';
+import { EricUIProvider } from '../src/EricUIProvider';
 
 const preview: Preview = {
   parameters: {
@@ -16,6 +17,13 @@ const preview: Preview = {
       test: 'todo',
     },
   },
+  decorators: [
+    (Story) => (
+      <EricUIProvider>
+        <Story />
+      </EricUIProvider>
+    )
+  ]
 };
 
 export default preview;
---
+++ src/EricUIProvider.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import './variables.css';
+
+export const EricUIProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <>{children}</>;
+};
