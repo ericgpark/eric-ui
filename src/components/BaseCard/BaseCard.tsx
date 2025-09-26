740498344b0fef3f93d3c071c3312a1294d55775
ericgpark <ericparkmusic@gmail.com>
2025-09-26 10:09:38 -0700

    Create BaseCard component

---
+++ src/components/BaseCard/BaseCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BaseText } from '../BaseText/BaseText';
+
+export interface BaseCardProps {
+  heading: string;
+  subheading: string;
+  footer: string;
+  children: any;
+}
+
+export const BaseCard: React.FC<BaseCardProps> = ({ heading, subheading, footer, children }) => {
+  return (
+    <div className="base-card">
+      <div className="base-card__header">
+        <BaseText type="heading">{heading}</BaseText>
+        <BaseText type="subheading">{subheading}</BaseText>
+      </div>
+
+      <div className="base-card__content">
+        {children}
+      </div>
+
+      <div className="base-card__footer">
+        {footer}
+      </div>
+    </div>
+  );
+};
