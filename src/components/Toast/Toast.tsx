792821a25b24d15858633087401c4cf479568c0e
ericgpark <ericparkmusic@gmail.com>
2025-10-07 10:58:33 -0700

    Add Toast component

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BaseText } from '../BaseText/BaseText';
+import { BaseButton } from '../BaseButton/BaseButton';
+import BaseIcon from '../BaseIcon/BaseIcon';
+
+export interface ToastProps {
+  type: 'success' | 'info' | 'warning' | 'error';
+  message: string;
+  onClose?: () => void;
+}
+
+export const Toast = ({ type, message, onClose }: ToastProps) => {
+  return (
+    <div className={`toast toast-${type}`}>
+      <BaseText type="body">
+        {message}
+      </BaseText>
+
+      {onClose && (
+        <BaseButton
+          variant="tertiary"
+          onClick={onClose}
+        >
+          <BaseIcon name="close" />
+        </BaseButton>
+      )}
+    </div>
+  );
+};
